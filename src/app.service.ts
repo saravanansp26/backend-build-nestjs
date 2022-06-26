7fc7df6502ddfb70a9f4c8dfbf47f1df8dbd9f81
Saravana Perumal <saravana.nsp26@gmail.com>
2022-06-26 13:22:02 +0530

    updated package.json

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log("application custom log 00001");
+    console.log(`PORT : ${process.env.PORT}`);
     return 'Hello World!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
+  console.log("application custom log 00001");
+  console.log(`PORT : ${PORT}`);
   await app.listen(PORT);
 }
 bootstrap();
